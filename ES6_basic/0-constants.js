bdffedea4e0ef724d1d5045166bb6fa2dd72b162
“Tristian <tristian.davis@atlasschool.com>
2025-03-05 11:39:28 -0600

    changing vscode tab/spacing

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -8,8 +8,8 @@ export function taskFirst() {
   }
 
   export function taskNext() {
-	let combination = 'But sometimes let';
-	combination += getLast();
+  let combination = 'But sometimes let';
+  combination += getLast();
 
-	return combination;
+  return combination;
   }
