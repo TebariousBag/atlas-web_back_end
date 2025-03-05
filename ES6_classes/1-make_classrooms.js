15a7e8c885469c505c7c07b10ef7111b22c3b114
“Tristian <tristian.davis@atlasschool.com>
2025-03-05 17:00:42 -0600

    fix lint

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,10 +1,10 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
 // export function instead of class
-export default function initializeRooms () {
-	return [
-	new ClassRoom(19),
-	new ClassRoom(20),
-	new ClassRoom(34),
-	];
+export default function initializeRooms() {
+  return [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+    ];
 }
