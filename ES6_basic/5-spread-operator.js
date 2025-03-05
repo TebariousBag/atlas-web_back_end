255b09aa7230178d1fe06538ebb17a518a269b29
“Tristian <tristian.davis@atlasschool.com>
2025-03-05 12:38:59 -0600

    forgot spread operator on string

--- ES6_basic/5-spread-operator.js
+++ ES6_basic/5-spread-operator.js
@@ -1,4 +1,4 @@
 export default function concatArrays(array1, array2, string) {
-  return [...array1, ...array2, string];
+  return [...array1, ...array2, ...string];
 }
 /* so this just unpacks the arrays */
