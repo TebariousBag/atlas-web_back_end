d4cabe1cf37c762f48a094fb35ba4763a404cc86
“Tristian <tristian.davis@atlasschool.com>
2025-03-05 14:01:56 -0600

    length of list

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,11 @@
+export default function createReportObject(employeesList) {
+    return {
+      allEmployees: {
+		...employeesList },
+		// just spread the list
+	  getNumberOfDepartments() {
+		return Object.keys(employeesList).length;
+		// just the length of the list after sprreading
+	  }
+	}
+}
