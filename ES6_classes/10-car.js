d08492f9474eb6cddbdb2ecdc3c565a9c0b96959
“Tristian <tristian.davis@atlasschool.com>
2025-03-07 13:45:50 -0600

    linter

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,23 +1,27 @@
 export default class Car {
-  constructor (brand, motor, color) {
+  constructor(brand, motor, color) {
   // private
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
+
   // getters
   get brand() {
     return this._brand;
   }
+
   get motor() {
     return this._motor;
   }
+
   get color() {
     return this._color;
   }
+
   // cloneCar method
   cloneCar() {
-    const car = this.constructor;
-    return new car(this._brand, this._motor, this._color);
+    const Tar = this.constructor;
+    return new Tar(this._brand, this._motor, this._color);
   }
 }
