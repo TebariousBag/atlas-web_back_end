da841361be5a81e36e7c52f2234db296d539bcd9
“Tristian <tristian.davis@atlasschool.com>
2025-03-07 16:50:25 -0600

    more linter, should have corrected as I went along

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,20 +1,21 @@
 import Building from './5-building';
 // note to self to remove .js from import
 export default class SkyHighBuilding extends Building {
-  constructor (sqft, floors) {
+  constructor(sqft, floors) {
   // call parent for sqft
-  super(sqft);
-  // validate first
-  if (typeof floors !== 'number') {
-    throw new TypeError('Floors must be a number');
-  }
-  // set private properties
-  this._floors = floors;
+    super(sqft);
+    // validate first
+    if (typeof floors !== 'number') {
+      throw new TypeError('Floors must be a number');
+    }
+    // set private properties
+    this._floors = floors;
   }
   // getters
   get floors() {
     return this._floors;
   }
+
   get sqft() {
     return this._sqft;
   }
