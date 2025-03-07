38193bf95fe5e8572750d4c6495a02717d06ffbf
“Tristian <tristian.davis@atlasschool.com>
2025-03-07 16:55:39 -0600

    airport linter

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,16 +1,17 @@
 export default class Airport {
-	constructor (name, code) {
+  constructor(name, code) {
     // validate first
     if (typeof name !== 'string') {
-      throw new TypeError ('Name must be a string');
+      throw new TypeError('Name must be a string');
     }
     if (typeof code !== 'string') {
-      throw new TypeError ('Code must be a string');
+      throw new TypeError('Code must be a string');
     }
     // set private properties
     this._name = name;
     this._code = code;
-	}
+  }
+
   // sybol toStringTag
   // return code
   get [Symbol.toStringTag]() {
