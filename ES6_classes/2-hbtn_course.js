b021f543db4bcd2f2d927f9a881e72c39f518e8d
“Tristian <tristian.davis@atlasschool.com>
2025-03-07 10:42:47 -0600

    i've made a mess of this

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,37 +1,55 @@
 export default class HolbertonCourse {
   // constructor method
   constructor(name, length, students) {
-    // name
-    this._name = name;
-
+    // Validate first
     if (typeof name !== 'string') {
-      throw TypeError('Name must be a string');
+      throw new TypeError('Name must be a string');
     }
-    // length
-    this._length = length;
-
     if (typeof length !== 'number') {
-      throw TypeError('Length must be a number');
+      throw new TypeError('Length must be a number');
     }
-    // students
-    this._students = students;
-    // I needed help with this part
-    // check if students is an array of strings
-    // Array.isArray() method determines whether the passed value is an Array.
     if (!Array.isArray(students) || !students.every(student => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
+    
+    // Then set properties
+    this._name = name;
+    this._length = length;
+    this._students = students;
   }
+  
   // getters
   get name() {
     return this._name;
   }
+  
   get length() {
     return this._length;
   }
+  
   get students() {
     return this._students;
   }
-  // setters
   
+  // setters
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+  
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = length;
+  }
+  
+  set students(students) {
+    if (!Array.isArray(students) || !students.every(student => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = students;
+  }
 }
