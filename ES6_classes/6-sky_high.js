46b226b3187f615106c7ffe2e04c8cb5a96c570e
“Tristian <tristian.davis@atlasschool.com>
2025-03-07 12:32:43 -0600

    works in terminal

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,25 @@
+import Building from './5-building';
+// note to self to remove .js from import
+export default class SkyHighBuilding extends Building {
+  constructor (sqft, floors) {
+  // call parent for sqft
+  super(sqft);
+  // validate first
+  if (typeof floors !== 'number') {
+    throw new TypeError('Floors must be a number');
+  }
+  // set private properties
+  this._floors = floors;
+  }
+  // getters
+  get floors() {
+    return this._floors;
+  }
+  get sqft() {
+    return this._sqft;
+  }
+  // override evacuationWarningMessage and return string
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
