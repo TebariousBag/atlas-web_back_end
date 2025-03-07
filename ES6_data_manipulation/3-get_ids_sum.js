cad73562202c9e6ddaf6d63ba7acef020f650710
“Tristian <tristian.davis@atlasschool.com>
2025-03-07 15:16:46 -0600

    added a return 0 and fixed linter

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,10 +1,10 @@
 export default function getStudentIdsSum(students) {
-	if (Array.isArray(students)) {
-		// reduce, using the theSum to hold the value. we are starting at 0
-		// thePerson is the current person in the array
-		// then we add thePerson.id to theSum and keep going until we have gone through all the people
-		return students.reduce((theSum, thePerson) => theSum + thePerson.id, 0);
+  if (Array.isArray(students)) {
+    // reduce, using the theSum to hold the value. we are starting at 0
+    // thePerson is the current person in the array
+    // then we add thePerson.id to theSum and keep going until we have gone through all the people
+    return students.reduce((theSum, thePerson) => theSum + thePerson.id, 0);
 
-    return [];
-	}
+    return 0;
+  }
 }
