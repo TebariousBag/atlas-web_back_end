b8be5b8b2390028daf5a3faa9616cc15a7fb51cc
“Tristian <tristian.davis@atlasschool.com>
2025-03-07 12:39:22 -0600

    toString return code

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,18 @@
+export default class Airport {
+	constructor (name, code) {
+    // validate first
+    if (typeof name !== 'string') {
+      throw new TypeError ('Name must be a string');
+    }
+    if (typeof code !== 'string') {
+      throw new TypeError ('Code must be a string');
+    }
+    // set private properties
+    this._name = name;
+    this._code = code;
+	}
+  // toString
+  toString () {
+    return this._code;
+  }
+}
