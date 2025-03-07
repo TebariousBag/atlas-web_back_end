9012f3e772ba8e5919ba02c0a556b055f9b453b4
“Tristian <tristian.davis@atlasschool.com>
2025-03-07 15:12:45 -0600

    reduce

---
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,8 @@
+export default function getStudentIdsSum(students) {
+	if (Array.isArray(students)) {
+		// reduce, using the theSum to hold the value. we are starting at 0
+		// thePerson is the current person in the array
+		// then we add thePerson.id to theSum and keep going until we have gone through all the people
+		return students.reduce((theSum, thePerson) => theSum + thePerson.id, 0);
+	}
+}
