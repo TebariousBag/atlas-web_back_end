f1237ac3c77fc15708e607fc6abcebf1d9389a43
“Tristian <tristian.davis@atlasschool.com>
2025-03-07 10:59:11 -0600

    set up constructor

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,38 @@
+export default class Currency {
+	constructor (code, name) {
+		// validate first
+      if (typeof code !== 'string') {
+		throw new TypeError('Code must be a string');
+	  }
+
+	  if (typeof name !== 'string') {
+		throw new TypeError('Name must be a string');
+	  }
+	  // set private properties
+	  this._name = name;
+	  this.code = code;
+}
+    // name getter setter
+	get name() {
+		return this._name;
+	}
+
+	set name(NewName) {
+		if (typeof NewName !== 'string') {
+			throw new TypeError('Name must be a string');
+		}
+		this._name = NewName;
+	}
+
+	// code getter setter
+	get code() {
+		return this._code;
+	}
+
+	set code(NewCode) {
+		if (typeof NewCode !== 'string') {
+			throw new TypeErro ('Code must be a string');
+		}
+		this._code = NewCode;
+	}
+}
