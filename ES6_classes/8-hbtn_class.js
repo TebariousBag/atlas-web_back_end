f461c6f2a1ad60a47e8051e64efb8bdf76d76b36
“Tristian <tristian.davis@atlasschool.com>
2025-03-07 12:57:06 -0600

    primitive

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,27 @@
+export default class HolbertonClass {
+	constructor (size, location) {
+    // validate first
+    if (typeof size !== 'number') {
+      throw new TypeError ('Size must be a number');
+    }
+    if (typeof location !== 'string') {
+      throw new TypeError ('Location must be a string');
+    }
+    // set private properties after validate
+    this._size = size;
+    this._location = location;
+  }
+  // getters
+  get size() {
+    return this._size;
+  }
+  get location() {
+    return this._location;
+  }
+  valueOf() {
+    return this._size;
+  }
+  toString() {
+    return this._location;
+  }
+}
