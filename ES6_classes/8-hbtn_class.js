80da34d6bc44bbb15c5cd2309899bb45091fcc04
“Tristian <tristian.davis@atlasschool.com>
2025-03-07 16:58:23 -0600

    8 linter

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -1,11 +1,11 @@
 export default class HolbertonClass {
-	constructor (size, location) {
+  constructor(size, location) {
     // validate first
     if (typeof size !== 'number') {
-      throw new TypeError ('Size must be a number');
+      throw new TypeError('Size must be a number');
     }
     if (typeof location !== 'string') {
-      throw new TypeError ('Location must be a string');
+      throw new TypeError('Location must be a string');
     }
     // set private properties after validate
     this._size = size;
@@ -15,12 +15,15 @@ export default class HolbertonClass {
   get size() {
     return this._size;
   }
+
   get location() {
     return this._location;
   }
+
   valueOf() {
     return this._size;
   }
+
   toString() {
     return this._location;
   }
