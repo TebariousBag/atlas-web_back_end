7e45c3e21c3f42819793b366ec018b55850621a7
“Tristian <tristian.davis@atlasschool.com>
2025-03-07 11:25:48 -0600

    validation getter setter so far

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,31 @@
+import Currency from './3-currency.';
+// no .js on import
+export default class Pricing {
+	constructor (amount, currency) {
+		// validate first
+		if (typeof amount !== 'number') {
+			throw new TypeError('Amount must be a number');
+		}
+
+		// private properties
+		this._currency = currency;
+		this._amount = amount;
+	}
+
+	// currency getter setter
+	get currency () {
+		return this._currency;
+	}
+
+	set currency (NewCurrency) {
+		this._currency = NewCurrency;
+	}
+
+	// amount getter setter
+	get amount() {
+		return this._amount = amount;
+	}
+
+	// displayFullPrice method
+	
+}
