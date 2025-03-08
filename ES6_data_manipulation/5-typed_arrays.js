b4006b88b9c7a4475baba5d2e7a9da93f3241c6f
“Tristian <tristian.davis@atlasschool.com>
2025-03-07 18:55:13 -0600

    can't use new int8? have to use dataview because test is outdated?

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -6,9 +6,9 @@ export default function createInt8TypedArray(length, position, value) {
   // use arraybuffer to get raw length of bytes, this is container for the data
   const buff = new ArrayBuffer(length);
   // view the buffer as int8, allows us to read and write to the buffer
-  const view = new Int8Array(buff);
+  const view = new DataView(buff);
   // set value at that index position we just got
-  view[position] = value;
+  view.setInt8(position, value);
   // return the view of the buffer, we will see the changes here
   return view;
 }
