c2e202c69d54ff2b547f5d5d99681f75dc545e57
“Tristian <tristian.davis@atlasschool.com>
2025-03-07 18:44:29 -0600

    int8

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,3 +1,14 @@
 export default function createInt8TypedArray(length, position, value) {
-  
+  // make sure position is in range starting at 0
+  if (position < 0 || position >= length) {
+    throw Error('Position outside range');
+  }
+  // use arraybuffer to get raw length of bytes, this is container for the data
+  const buff = new ArrayBuffer(length);
+  // view the buffer as int8, allows us to read and write to the buffer
+  const view = new Int8Array(buff);
+  // set value at that index position we just got
+  view[position] = value;
+  // return the vie of the buffer, but we will see the changes
+  return buff;
 }
