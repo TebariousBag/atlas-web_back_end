766628cdefdee2250db6248a6cf240cb21e0fbdc
“Tristian <tristian.davis@atlasschool.com>
2025-03-07 18:59:15 -0600

    new Set

---
+++ ES6_data_manipulation/6-set.js
@@ -0,0 +1,5 @@
+export default function setFromArray(theArray) {
+  // Set removes duplicates and preserves order of first occurence
+  // maintains unique values only
+  return new Set(theArray);
+}
