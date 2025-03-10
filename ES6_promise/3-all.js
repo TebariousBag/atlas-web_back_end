fd86a06ac290ee9c8b40c6bf89825105e4c73230
“Tristian <tristian.davis@atlasschool.com>
2025-03-10 15:34:37 -0500

    Promise.all

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,15 @@
+import { uploadPhoto, createUser } from './utils.js';
+
+export default function handleProfileSignup() {
+  // Promise.all one promise that resolves when both promises are resolved
+  return Promise.all([uploadPhoto(), createUser()])
+    // then is how we handle success cases
+    // print body and names
+    .then(([uploadPhoto, createUser]) => {
+      console.log('${Uploadphoto.body} ${createUser.firstName} ${createUser.lastName}');
+    })
+    // return error if any promise fails
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
