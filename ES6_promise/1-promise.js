81465b9620135a254dacc8641a860bc6b88e8be0
“Tristian <tristian.davis@atlasschool.com>
2025-03-10 14:30:31 -0500

    added an if else

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,13 +1,15 @@
 export default function getFullResponseFromAPI(success) {
-	// function returns a Promise
-	// Promise, has 2 parameters, a resolution and a rejection
-	// in this case its just empty responses
-	return new Promise((resolved, rejected) => {
-	  resolved({
-		status: 200,
-		body: 'Success'
-	  });
-	  rejected(new Error('The fake API is not working currently'));
-	});
-  }
-  
+  // function returns a Promise
+  // Promise, has 2 parameters, a resolution and a rejection
+  // in this case its just empty responses
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success'
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
