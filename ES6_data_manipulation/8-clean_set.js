7f7d95e5bb7c2d487a768b89f70e0694c3da226c
“Tristian <tristian.davis@atlasschool.com>
2025-03-10 13:44:19 -0500

    clean set

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,3 +1,19 @@
 export default function cleanSet(set, startString) {
-  
+  // if startString is not string, or is empty, return empty string
+  if (!startString || typeof startString !== 'string') {
+    return '';
+  }
+  // empty array to hold value
+  const holder = [];
+  // loop through set and see if it starts with startString
+  for (const i of set) {
+    if (typeof i === 'string' && i.startsWith(startString)) {
+      // if it does, push it to holder array
+      // slice the startString from the string
+      // slicing the length of characters it is
+      holder.push(i.slice(startString.length));
+    }
+  }
+  // return holder joined by -
+  return holder.join('-');
 }
