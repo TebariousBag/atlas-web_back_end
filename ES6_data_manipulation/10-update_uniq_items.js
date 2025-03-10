f232792eed62c3ca745e194aef1e583084c23f3b
“Tristian <tristian.davis@atlasschool.com>
2025-03-10 13:07:45 -0500

    update unique items

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,16 @@
+export default function updateUniqItems(map) {
+  // check if arg is a map
+  if (map instanceof Map) {
+    // iterate through map and update all 1's to 100
+    for (const [key, value] of map) {
+      if (value === 1) {
+        map.set(key, 100);
+      }
+    }
+    return map;
+  }
+  // if arg is not a map, throw error
+  else {
+    throw new Error('Cannot process');
+  }
+}
