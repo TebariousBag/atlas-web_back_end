650314ddc89eb548eca5682a89879f837515baa4
“Tristian <tristian.davis@atlasschool.com>
2025-03-11 11:23:41 -0500

    missing semicolon

--- ES6_promise/7-load_balancer.js
+++ ES6_promise/7-load_balancer.js
@@ -1,5 +1,5 @@
 export default function loadBalancer(chinaDownload, USDownload) {
   // Promise.race takes mutiple promises and returns the first one to settle
   // whichever settles first, the first one to resolve or reject
-  return Promise.race([chinaDownload, USDownload])
+  return Promise.race([chinaDownload, USDownload]);
 }
