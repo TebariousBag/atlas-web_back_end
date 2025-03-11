0b0e0eb42dff668f86da523f1dd8c2c00abc85d5
“Tristian <tristian.davis@atlasschool.com>
2025-03-11 11:29:17 -0500

    throw error

---
+++ ES6_promise/8-try.js
@@ -0,0 +1,7 @@
+export default function divideFunction(numerator, denominator) {
+  // pretty self explanatory, if denominator is 0, throw an error
+  if (denomiinator === 0) {
+    throw new Error('Cannot divide by 0');
+  }
+  return numerator / denominator;
+}
