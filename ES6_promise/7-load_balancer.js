a933aabc8220032d5626d82116d4a34bcd253c35
“Tristian <tristian.davis@atlasschool.com>
2025-03-11 11:23:01 -0500

    .race

---
+++ ES6_promise/7-load_balancer.js
@@ -0,0 +1,5 @@
+export default function loadBalancer(chinaDownload, USDownload) {
+  // Promise.race takes mutiple promises and returns the first one to settle
+  // whichever settles first, the first one to resolve or reject
+  return Promise.race([chinaDownload, USDownload])
+}
