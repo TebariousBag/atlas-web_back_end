385ae6d9df832c9c89ed5cc338295499135afeb8
“Tristian <tristian.davis@atlasschool.com>
2025-03-11 11:09:47 -0500

    put rejected in different format?

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -10,6 +10,6 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
     // if status is rejected, value is result.reason.message
     .then((results) => results.map((result) => ({
       status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason.message,
+      value: result.status === 'fulfilled' ? result.value : `Error: ${result.reason.message}`,
     })));
   }
