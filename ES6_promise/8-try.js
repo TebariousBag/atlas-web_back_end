7e21dfe42788b0756edc5107d9e9097803f76c9e
“Tristian <tristian.davis@atlasschool.com>
2025-03-11 11:32:24 -0500

    typo

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,6 +1,6 @@
 export default function divideFunction(numerator, denominator) {
   // pretty self explanatory, if denominator is 0, throw an error
-  if (denomiinator === 0) {
+  if (denominator === 0) {
     throw new Error('Cannot divide by 0');
   }
   return numerator / denominator;
