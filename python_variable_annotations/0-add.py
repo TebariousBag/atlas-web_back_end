fa47539eba41f9f642a96ae5fb5b0f340cd2ef5e
“Tristian <tristian.davis@atlasschool.com>
2025-05-08 17:40:33 -0500

    pycode

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -5,8 +5,9 @@ type-annotated function add that takes a float a and
 a float b as arguments and returns their sum as a float
 """
 
+
 def add(a: float, b: float) -> float:
-	"""
-	return sum of two float ints
-	"""
-	return a + b
+    """
+    return sum of two float ints
+    """
+    return a + b
