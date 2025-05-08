309d05a050d079703bab922a4c3478110e482fcc
“Tristian <tristian.davis@atlasschool.com>
2025-05-08 18:12:46 -0500

    float to string, runs in terminal

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+type-annotated function to_str that takes a float n
+as argument and returns the string representation of the float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    takesn float n and returns string
+    representation of n
+    """
+    return (str(n))
