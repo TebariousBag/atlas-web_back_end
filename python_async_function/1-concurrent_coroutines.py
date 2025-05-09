f4b51071a8adc2c2ec5cb6a956d1b09c88814670
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-05-09 21:55:50 +0000

    whitespace on blank lines

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -10,11 +10,11 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Run wai_random n times with max_delay as arg
-    
+
     Args:
         n (int): times to run wait_random
         max_delay: the time of delay in seconds
-        
+
     Returns:
         List[float]: list of all the delays
     """
@@ -23,7 +23,7 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     # create task to run it n times and save to temp
     for i in range(n):
         temp.append(asyncio.create_task(wait_random(max_delay)))
-    
+
     results = []
     # iterate through each completed task in temp
     for each_completed in asyncio.as_completed(temp):
@@ -31,5 +31,5 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
         delay = await each_completed
         # and then add that delay to the results
         results.append(delay)
-        
+
     return (results)
