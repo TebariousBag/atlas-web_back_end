ca8261bbb1ee930c22842dbe6b0e82b9040fe677
“Tristian <tristian.davis@atlasschool.com>
2025-05-09 13:30:44 -0500

    line too long

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-type-annotated function sum_mixed_list which takes a list mxd_lst of integers and floats and returns their sum as a float
+type-annotated function sum_mixed_list which takes a list mxd_lst
+of integers and floats and returns their sum as a float
 """
 from typing import List, Union
 
