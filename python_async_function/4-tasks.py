fece85ff30fb2b09cf9c9b461b5d2677363be43b
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-05-09 22:54:08 +0000

    just change to task_wait_n ?

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+function that creates and returns task wait_random(max_delay)
+"""
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Run task_wai_random n times with max_delay as arg
+
+    Args:
+        n (int): times to run task_wait_random
+        max_delay: the time of delay in seconds
+
+    Returns:
+        List[float]: list of all the delays
+    """
+    # to hold each time we run wait_random(max_delay)
+    temp = []
+    # create task to run it n times and save to temp
+    for i in range(n):
+        temp.append(task_wait_random(max_delay))
+
+    results = []
+    # iterate through each completed task in temp
+    for each_completed in asyncio.as_completed(temp):
+        # save the delay as we iterate through each time
+        delay = await each_completed
+        # and then add that delay to the results
+        results.append(delay)
+
+    return (results)
