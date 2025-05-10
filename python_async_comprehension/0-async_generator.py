77c2fa200d75606a2674b60dcb6ba6e5ecaa54ec
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-05-10 00:10:35 +0000

    thought this should work, only getting three values

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env
+"""
+coroutine called async_generator will loop 10 times,
+each time asynchronously wait 1 second,
+then yield a random number between 0 and 10
+"""
+import asyncio
+import random
+
+
+async def async_generator() -> float:
+    """
+    functtion will loop 10 times, each time wait 1 second then yield
+    a random num from 0 - 10
+    """
+    count = 0
+    # iterate 10 times
+    while count < 10:
+        count+= 1
+        # sleep for 1 second
+        await asyncio.sleep(1)
+        # then random number between 0 - 10
+        # yield pauses here and returns it to whatever
+        # is calling for it, we can continue the
+        # loop from here later
+        yield random.uniform(0, 10)
