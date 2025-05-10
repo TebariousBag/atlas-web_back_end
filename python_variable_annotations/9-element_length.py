99ae73e81a741be66c23b67b80864d3b17410a7f
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-05-10 01:22:39 +0000

    white space on blank line

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -9,7 +9,7 @@ from typing import List, Tuple, Sequence, Iterable
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
     Annotate
-    
+
     Args:
         lst(Iterable[Sequence]): an iterable of sequences
 
