26b06385f5dae3d4106248b748980a60195cb9e8
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-05-10 01:21:30 +0000

    came back to this, didn't realize the answer is essentially given to us

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 """
-
+Annotate the below function’s parameters
+and return values with the appropriate types
 """
 from typing import List, Tuple, Sequence, Iterable
 
 
-def element_length()
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Annotate
+    
+    Args:
+        lst(Iterable[Sequence]): an iterable of sequences
+
+    Returns:
+        List[Tuple[Sequence, int]]: list of tuples
+    """
+    return [(i, len(i)) for i in lst]
