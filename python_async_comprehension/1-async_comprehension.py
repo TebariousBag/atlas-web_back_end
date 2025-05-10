24a7f86b79091e8267d050783d28b4dcb2588b56
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-05-10 01:03:20 +0000

    why did I have to change up my async for loop?

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -11,11 +11,11 @@ async_generator = __import__('0-async_generator').async_generator
 async def async_comprehension() -> List[float]:
     """
     collect 10 random nums from async_generator
+
+    Returns:
+        List[float]: list of 10 random numbers
+        from async_generator
     """
-    # empty list
-    the_list = []
     # iterate over async_generator and add to list
     # use async for, just like we use async def
-    async for randn in async_generator():
-        the_list.append(randn)
-    return (the_list)
+    return [randn async for randn in async_generator()]
---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+coroutine that will execute async_comprehension
+four times in parallel using asyncio.gather
+"""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    rrun async_comprehension 4 times and find out how long it takes with gather
+    
+    Returns:
+        float: total time it took to run 4 times
+    """
+    # start timer
+    start = time.time()
+    # gather 4 times, but wait for all 4 to gather first
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+    # end timer
+    end = time.time()
+    # get total
+    total_time = end - start
+    return (total_time)
