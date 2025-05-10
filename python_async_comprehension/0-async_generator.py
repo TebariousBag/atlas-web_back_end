95ca8d7f8d4adb5dd08402b2f3ba2182e19a9af8
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-05-10 00:14:11 +0000

    return type was just float, not Generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,9 +6,10 @@ then yield a random number between 0 and 10
 """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator() -> float:
+async def async_generator() -> Generator[float, None, None]:
     """
     functtion will loop 10 times, each time wait 1 second then yield
     a random num from 0 - 10
