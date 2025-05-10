8bcf097ecf11fed7f4e4cf579dbafbb722c1052f
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-05-10 01:08:00 +0000

    pycodestyle fixes

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -11,17 +11,15 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """
     rrun async_comprehension 4 times and find out how long it takes with gather
-    
+
     Returns:
         float: total time it took to run 4 times
     """
     # start timer
     start = time.time()
     # gather 4 times, but wait for all 4 to gather first
-    await asyncio.gather(async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension())
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                         async_comprehension(), async_comprehension())
     # end timer
     end = time.time()
     # get total
