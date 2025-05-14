7c1f292463920bdbc360a5ab349219fd1767b02a
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-05-14 06:42:36 +0000

    FIFO, runs in terminal

---
+++ caching/1-fifo_cache.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+class FIFOCache that inherits from BaseCaching and is a caching system
+"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    Class to FIFO data
+	"""
+    # inherit from parent BaseCaching
+    def __init__(self):
+        super().__init__()
+    
+    def put(self, key, item):
+        """
+        adds an item to the cache
+        """
+        # if there is a key and an item
+        # value of key is the value of item
+        if key and item:
+            self.cache_data[key] = item
+            # if number of items in cache_data is more than max_items
+            # then we del the first
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                # iterate through keys, and break right at first key
+                for keys in self.cache_data:
+                    first = keys
+                    break
+                print("DISCARD:", first)
+                # then delete the first key we just located
+                del self.cache_data[first]
+
+    def get(self, key):
+        """
+        get an item from the cache by key
+        """
+        # when using get method, if key is not there then it returns none
+        return (self.cache_data.get(key))
