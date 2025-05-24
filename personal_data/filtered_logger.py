821a4f44bed91991dafd52526fb56edaeea673ab
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-05-24 17:20:58 +0000

    logging is confusing

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ module redacts sensitive information
 """
 
 import re
+import logging
 from typing import List
 
 
@@ -20,3 +21,30 @@ def filter_datum(fields: List[str], redaction: str,
     pattern = fr"({joined_fields})={value}"
     # substitute pattern, with redacted message
     return (re.sub(pattern, rf"\1={redaction}", message))
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    # takes fields as arg
+    def __init__(self, fields):
+        """
+        fields to redact
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        redact sensitive info from message
+        """
+        # take our record message, which is part of logging
+        # then run our filter on the message
+        record.msg = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        # return the logging record in original format
+        # it's just filtered now
+        return (super().format(record))
