89d114f809515a6da2f6b61d86cf77625702276a
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-05-24 20:06:17 +0000

    is valid

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -6,6 +6,14 @@ encrypt passwords with bcrypt
 import bcrypt
 
 
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ 
+    validate that provided password matches hashed password
+    """
+    # checkpw, just check if encoded pw matches hashed password
+    return bcrypt.checkpw(password.encode(), hashed_password)
+
+
 def hash_password(password: str) -> bytes:
     """ return a hashed password """
     # random data added to your password before hashing
