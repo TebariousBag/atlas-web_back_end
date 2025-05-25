1a5a8affccff4670b87a9a0ed0598265da7c9661
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-05-25 18:34:59 +0000

    extract base_64, just following steps

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,4 +10,19 @@ class BasicAuth(Auth):
     """
     class BasicAuth that inherits from Auth
     """
-    pass
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+        returns the Base64 part of the Authorization
+        """
+        if authorization_header is None:
+            return None
+
+        if not isinstance(authorization_header, str):
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        # split after the first 6 characters
+        return authorization_header[6:]
