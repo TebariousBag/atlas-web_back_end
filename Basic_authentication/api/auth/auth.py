24fb52c5b3f6cfd9ab2c42b2bc5c1e6fd898281a
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-05-25 16:44:16 +0000

    template for auth

---
+++ Basic_authentication/api/auth/auth.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+"""
+
+from flask import request
+from typing import List, Type, TypeVar
+
+
+class Auth:
+    """
+    api authentications
+    """
+    
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        does it require authentication
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        returns header request
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        returns current user
+        """
+        return None
