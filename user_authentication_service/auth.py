427606c7c99281fea33ac024b729b2d1575cca76
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-05-26 21:41:47 +0000

    function

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -7,14 +7,14 @@ arguments and returns bytes
 import bcrypt
 
 
-def _hash_password(self, password: str) -> bytes:
-        """
-        takes in a password string arguments and returns bytes
-        """
-        # encode to bytes
-        encoded_pw = password.encode('utf-8')
-        # var to salt
-        salt = bcrypt.gensalt()
-        # then hash it with salt
-        hashed = bcrypt.hashpw(encoded_pw, salt)
-        return hashed
+def _hash_password(password: str) -> bytes:
+    """
+    takes in a password string arguments and returns bytes
+    """
+    # encode to bytes
+    encoded_pw = password.encode('utf-8')
+    # var to salt
+    salt = bcrypt.gensalt()
+    # then hash it with salt
+    hashed = bcrypt.hashpw(encoded_pw, salt)
+    return hashed
