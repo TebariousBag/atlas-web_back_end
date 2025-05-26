8839e582ab4b1cfebbc6f311f2dc1da8eac3780b
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-05-26 18:03:11 +0000

    create session with uuid

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -4,10 +4,29 @@ session auth module
 """
 
 from api.v1.auth.auth import Auth
-
+import uuid
 
 class SessionAuth(Auth):
     """
     inherits from Auth
     """
-    pass
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        creates a Session ID for a user_id
+        """
+        if user_id is None:
+            return None
+
+        if not isinstance(user_id, str):
+            return None
+
+        # creating session id
+        session_id = str(uuid.uuid4())
+
+        # store the session
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
