34a2266b8f5c4186ba98c04df9fafb87c381b470
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-05-26 23:24:41 +0000

    destroy session, first part

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -44,3 +44,21 @@ def login() -> str:
     response.set_cookie(session_name, session_id)
 
     return response
+
+def destroy_session(self, request=None) -> bool:
+    """
+    Deletes the user session (logout)
+    """
+    if request is None:
+        return False
+
+    session_id = self.session_cookie(request)
+    if session_id is None:
+        return False
+
+    user_id = self.user_id_for_session_id(session_id)
+    if user_id is None:
+        return False
+
+    del self.user_id_by_session_id[session_id]
+    return True
