eda9d77e34c42786d06c6277180a677fee02ad01
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-05-26 20:06:44 +0000

    and pycode

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -10,15 +10,15 @@ Base = declarative_base()
 
 
 class User(Base):
-	"""
-	class for user table
-	"""
-	__tablename__ = 'users'
+    """
+    class for user table
+    """
+    __tablename__ = 'users'
 
-	# defining table, mainly names and types
-	# need string lengths string lengths
-	id = Column(Integer, primary_key=True)
-	email = Column(String(250), nullable=False)
-	hashed_password = Column(String(250), nullable=False)
-	session_id = Column(String(250), nullable=True)
-	reset_token = Column(String(250), nullable=True)
+    # defining table, mainly names and types
+    # need string lengths string lengths
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
