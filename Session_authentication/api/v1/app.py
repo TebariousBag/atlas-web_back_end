2e6e4069dca973c4506120c2ccc4f0f8b5541717
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-05-26 18:31:24 +0000

    i had 2 abort 401

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -45,15 +45,12 @@ def before_request_func():
         '/api/v1/auth_session/login/'
     ]
 
-    if (auth.authorization_header(request) is None and
-            auth.session_cookie(request) is None):
-        abort(401)
-
     # auth require_auth of request.path and path_list
     if not auth.require_auth(request.path, path_list):
         return
 
-    if auth.authorization_header(request) is None:
+    if (auth.authorization_header(request) is None and
+            auth.session_cookie(request) is None):
         abort(401)
 
     # check for valid user
