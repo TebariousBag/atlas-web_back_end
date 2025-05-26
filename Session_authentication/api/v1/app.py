4cff2bd6caf6ef7194d9b68e92ad493eca4b05fa
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-05-26 17:42:18 +0000

    user id == me

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -50,8 +50,12 @@ def before_request_func():
     if auth.authorization_header(request) is None:
         abort(401)
 
-    if auth.current_user(request) is None:
+    # check for valid user
+    user = auth.current_user(request)
+    if user is None:
         abort(403)
+    # now current user is valid
+    request.current_user = user
 
 
 @app.errorhandler(404)
--- Session_authentication/api/v1/views/users.py
+++ Session_authentication/api/v1/views/users.py
@@ -25,7 +25,7 @@ def view_one_user(user_id: str = None) -> str:
       - User object JSON represented
       - 404 if the User ID doesn't exist
     """
-    if user_id is None:
+    if user_id == "me":
         abort(404)
     user = User.get(user_id)
     if user is None:
