f5755352762cfe0a521f89fa1703179008409af7
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-05-26 22:48:10 +0000

    going step by step on this one

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -2,3 +2,22 @@
 """
 Flask view that handles all routes for the Session authentication
 """
+from api.v1.views import app_views
+from flask import request, jsonify, abort
+from models.user import User
+import os
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login():
+    """
+    handles the user login
+    """
+    # use request form get to retrieve
+    email = request.form.get('email')
+    if not email:
+        return jsonify({"error": "email missing"}), 400
+    # use request form get to retrieve
+    password = request.form.get('password')
+    if not password:
+        return jsonify({"error": "password missing"}), 400
