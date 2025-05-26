27316e1031512aab6765f2c6fc64c43316cc0c0a
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-05-26 19:47:59 +0000

    setting up user model

---
+++ user_authentication_service/user.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+model named User for a database table named users
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+# base class for our catalog of classes
+Base = declarative_base()
+
+
+class User(Base):
+	"""
+	class for user table
+	"""
+	__tablename__ = 'users'
+
+	# defining table, mainly names and types
+	# i didn't use string lengths
+	id = Column(Integer, primary_key=False)
+	email = Column(String, nullable=False)
+	hashed_password = Column(String, nullable=False)
+	session_id = Column(String, nullable=True)
+	reset_token = Column(String, nullable=True)
