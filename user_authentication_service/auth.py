9a8d03c84871ab0b69aa81edb25cb8996e7a4e28
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-05-28 01:24:36 +0000

    register user

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -5,7 +5,31 @@ arguments and returns bytes
 """
 
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.exc import NoResultFound
 
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        register user to db
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            # hash pw with our hash pw function
+            hashed_pass = _hash_password(password)
+            # new user and add tto db
+            user = self._db.add_user(email, hashed_pass)
+            return user
 
 def _hash_password(password: str) -> bytes:
     """
