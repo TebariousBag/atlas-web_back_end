8630a66b2d52068cf7c5cd003cbe4a9232c7b82a
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-05-28 01:58:16 +0000

    basic flask

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,3 +2,17 @@
 """
 setup for basic flask app
 """
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def welcome():
+    """
+    welcome message
+    """
+    return jsonify({"message": "Bienvenue"})
+
+if __name__ == "__main__":
+    # i dont like port 5000 while using mac
+    app.run(host="0.0.0.0", port=5000)
