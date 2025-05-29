dfa5e10757a69890411d732c15678defb93baa74
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-05-29 03:48:31 +0000

    register new user

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,9 +2,14 @@
 """
 setup for basic flask app
 """
-from flask import Flask, jsonify
+from auth import Auth
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+from auth import Auth
+
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"])
@@ -14,6 +19,20 @@ def welcome():
     """
     return jsonify({"message": "Bienvenue"})
 
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """
+    end-point to register a user
+    """
+    # request for email and password
+    email = request.form['email']
+    password = request.form['password']
+    try:
+        # try registering it and return jsonified message
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 if __name__ == "__main__":
     # i dont like port 5000 while using mac
