6cd445461f2bf8bf650a18c402001668f1b132b9
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-06-02 16:59:03 +0000

    pushing test 1 and 2

---
+++ Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+unittests
+"""
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """
+    class for tests
+    """
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+
+    def test_access_nested_map(self, nested_map, path, expected):
+        """parameterization to test nested map"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError)
+    ])
+
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """test with keyerror exception"""
+        with self.assertRaises(expected):
+            access_nested_map(nested_map, path)
