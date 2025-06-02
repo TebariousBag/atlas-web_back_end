16ba66fa0cec6e8a684185919478a755923417fd
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-06-02 21:26:41 +0000

    propert mock, so far all tests are OK

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -6,7 +6,7 @@ import unittest
 from parameterized import parameterized
 from client import GithubOrgClient
 from unittest.mock import patch
-
+from unittest.mock import patch, PropertyMock
 
 class TestGithubOrgClient(unittest.TestCase):
     """
@@ -33,3 +33,23 @@ class TestGithubOrgClient(unittest.TestCase):
         # and assert that correct result was called
         mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
         self.assertEqual(result, {"login": org_name})
+
+    @patch('client.GithubOrgClient.org', new_callable=PropertyMock)
+    # make sure to take mock_org as argument
+    def test_public_repos_url(self, mock_org):
+        """
+        test githubOrgClient._public_repos_url
+        """
+        # set the return value we are expecting
+        mock_org.return_value = {
+            'repos_url': 'https://api.github.com/orgs/google/repos'
+    }
+        # set an instance of github org client for google
+        githubOrg = GithubOrgClient('google')
+        # should read the .org dict we made
+        result = githubOrg._public_repos_url
+        # assert that the result matches
+        self.assertEqual('https://api.github.com/orgs/google/repos', result)
+
+if __name__ == '__main__':
+    unittest.main()
