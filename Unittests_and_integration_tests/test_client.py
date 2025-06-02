9325e9c4429ea634212ed7f6f3af255bd5e184f1
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-06-02 21:13:56 +0000

    github org client

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -9,15 +9,27 @@ from unittest.mock import patch
 
 
 class TestGithubOrgClient(unittest.TestCase):
-	"""
-	TestGithubOrg class
-	"""
-	# inputs
-	@parameterized.expand([
-		("google"),
-		("abc"),
-	])
-	# mock json get
-	@patch("client.get_json")
-	def test_org(self, org_name, mock.get_json):
-		
+    """
+    TestGithubOrg class
+    """
+    # inputs
+    @parameterized.expand([
+        ("google",),
+        ("abc",),
+    ])
+    # mock json get
+    @patch("client.get_json")
+    def test_org(self, org_name, mock_get_json):
+        """
+        test github org client
+        """
+        # we want to return this
+        mock_get_json.return_value = {"login": org_name}
+        # client
+        client = GithubOrgClient(org_name)
+        # result
+        result = client.org
+        # make sure it was called once
+        # and assert that correct result was called
+        mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
+        self.assertEqual(result, {"login": org_name})
