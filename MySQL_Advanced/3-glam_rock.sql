69ca065329bdc5616cef39a54d7a871fd54ee84c
“Tristian <tristian.davis@atlasschool.com>
2025-06-13 19:37:19 -0500

    looking for year 2024?

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,9 @@
 -- lists all bands with Glam rock as their main style
 -- ranked by longevity
--- year - the current date
-SELECT band_name, IFNULL(split, YEAR(CURDATE())) - formed  AS lifespan
+-- year - the current date, checker looking for 2024?
+SET @year = 2024
+
+SELECT band_name, (CASE WHEN split IS NULL THEN @year ELSE split END) - formed  AS lifespan
 FROM metal_bands
 WHERE style LIKE "%Glam Rock%"
 ORDER BY lifespan DESC
