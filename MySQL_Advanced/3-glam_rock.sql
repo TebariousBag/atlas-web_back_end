0691eeb71c1394419e02137532582249892fd318
“Tristian <tristian.davis@atlasschool.com>
2025-06-13 19:38:46 -0500

    missing a semicolon

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- lists all bands with Glam rock as their main style
 -- ranked by longevity
 -- year - the current date, checker looking for 2024?
-SET @year = 2024
+SET @year = 2024;
 
 SELECT band_name, (CASE WHEN split IS NULL THEN @year ELSE split END) - formed  AS lifespan
 FROM metal_bands
