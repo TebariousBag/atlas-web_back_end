49e0c147a5b0dd6ad5c7408870103f92fa38817d
“Tristian <tristian.davis@atlasschool.com>
2025-06-23 15:48:37 -0500

    followed the hello world tutorial

---
+++ Node_JS/4-http.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// get http tools
+const http = require('http');
+// creating the server, request and respond
+const app = http.createServer((req, res) => {
+	// success response
+	res.statusCode = 200;
+	// tells it response is gonna be text
+	res.setHeader('Content-Type', 'text/plain');
+	// then we end response with our text
+	res.end('Hello World')
+});
+// listen on port 1245
+app.listen(1245);
+// export app function
+module.exports = app;
