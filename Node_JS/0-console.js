1a8c5beb16e4060cd62afda10e2cc77153bea650
“Tristian <tristian.davis@atlasschool.com>
2025-06-23 11:54:44 -0500

    export outside of main, and specify where with shebang

--- Node_JS/0-console.js
+++ Node_JS/0-console.js
@@ -1,7 +1,11 @@
+#!/usr/bin/node
+
+
 function displayMessage(theMessage) {
 	// to standard output
 	// console.log is stdout with newline
 	process.stdout.write(theMessage);
 
-	module.exports = theMessage;
 }
+
+	module.exports = displayMessage;
