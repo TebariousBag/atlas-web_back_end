d46ac73c7b44ff48dcce950cff0ec91a2d54cd25
“Tristian <tristian.davis@atlasschool.com>
2025-06-23 11:58:57 -0500

    fixed and figuring out lint

--- Node_JS/0-console.js
+++ Node_JS/0-console.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 
-
 function displayMessage(theMessage) {
-	// to standard output
-	// console.log is stdout with newline
-	process.stdout.write(theMessage);
-
+  // to standard output
+  // console.log is stdout with newline
+  process.stdout.write(theMessage);
 }
-
-	module.exports = displayMessage;
+// export function name
+module.exports = displayMessage;
