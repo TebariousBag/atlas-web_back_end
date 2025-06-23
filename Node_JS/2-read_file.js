4595da7a088da4c34f96b1e6c61e87ee19d7891c
“Tristian <tristian.davis@atlasschool.com>
2025-06-23 14:14:34 -0500

    fixed eslint, displays the data unfiltered and throws an error

--- Node_JS/2-read_file.js
+++ Node_JS/2-read_file.js
@@ -3,11 +3,18 @@
 const fs = require('fs');
 
 function countStudents(path) {
+  try {
   // read sycnchnously
   // save to variable so we can log it
-  const data = fs.readFileSync(path, 'utf-8');
-  console.log(data);
+    const data = fs.readFileSync(path, 'utf-8');
+    console.log(data);
+
+  // catching case of error,
+  } catch (err) {
+    // If reading fails, throw custom error
+    throw new Error('Cannot load the database');
+  }
 }
 
 // remember to export the function
-module.exports = countStudents
+module.exports = countStudents;
