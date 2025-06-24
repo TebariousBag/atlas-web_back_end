c15c07f31be6eb45be9735f44a7bc331571282b2
“Tristian <tristian.davis@atlasschool.com>
2025-06-24 15:50:42 -0500

    so much splitting and trimming

--- Node_JS/2-read_file.js
+++ Node_JS/2-read_file.js
@@ -13,12 +13,33 @@ function countStudents(path) {
   // split everytime there is a newline character
   const splitData = dataFromDb.trim().split('\n');
 
-  // the first header row is firstname,lastname,age,field
+  
+  // the first header row is firstName,lastname,age,field
   // that will be included in count, so we need to - 1
   const totalStudents = splitData.length - 1;
   console.log(`Number of students: ${totalStudents}`);
-}
 
+  const studInField = {};
+  // loop through all split data, start at one to skip header
+  for (let i = 1; i < splitData.length; i++) {
+    // split at , and assign and trim data for each respectively
+    const column = splitData[i].split(',');
+    const firstName = column[0].trim();
+    const field = column[3].trim();
+
+    // if there is no field, then leave it blank
+    if (!studInField[field]) {
+      studInField[field] = [];
+    }
+    // push the name of student to the field
+    studInField[field].push(firstName);
+  }
+  // respond with the message
+  for (const field in studInField) {
+    const students = studInField[field];
+    console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+  }
+}
 
 // remember to export the function
 module.exports = countStudents;
