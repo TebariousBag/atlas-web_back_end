fc3c01e0c360983f5bf2b62862797fc1d1aae07b
“Tristian <tristian.davis@atlasschool.com>
2025-06-24 15:23:50 -0500

    i got total student count to work, need to figure out the fields part

--- Node_JS/2-read_file.js
+++ Node_JS/2-read_file.js
@@ -3,18 +3,22 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  try {
-  // read sycnchnously
-  // save to variable so we can log it
-    const data = fs.readFileSync(path, 'utf-8');
-    console.log(data);
-
-  // catching case of error,
-  } catch (err) {
-    // If reading fails, throw custom error
+  // if fails throw error
+  if (!fs.existsSync(path)) {
     throw new Error('Cannot load the database');
   }
+  // read file from database.csv
+  const dataFromDb = fs.readFileSync(path, 'utf-8');
+  // from our data trim whitespace from beginning and end
+  // split everytime there is a newline character
+  const splitData = dataFromDb.trim().split('\n');
+
+  // the first header row is firstname,lastname,age,field
+  // that will be included in count, so we need to - 1
+  const totalStudents = splitData.length - 1;
+  console.log(`Number of students: ${totalStudents}`);
 }
+
 
 // remember to export the function
 module.exports = countStudents;
