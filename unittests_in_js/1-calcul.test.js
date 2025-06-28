9626362f00ae99b858e0874c7580b9016d83a3d4
“Tristian <tristian.davis@atlasschool.com>
2025-06-28 12:26:33 -0500

    combine descriptions

---
+++ unittests_in_js/1-calcul.test.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+// set up calculator, and seperate function testing for each operatpr
+describe('calculateNumber', function() {
+  // SUM testing
+	describe('SUM', function() {
+		it('should add 2 values', function() {
+			assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
+		})
+	});
+  // SUBTRACT testing
+	describe('SUBTRACT', function() {
+		it('should subtract 2 values', function() {
+			assert.strictEqual(calculateNumber('SUBTRACT', 1, 3), -2);
+		})
+	});
+  // DIVISION TESTING
+	describe('DIVIDE', function() {
+		it('should subtract 2 values', function() {
+			assert.strictEqual(calculateNumber('DIVIDE', 4, 2), 2);
+		})
+	});
+
+});
