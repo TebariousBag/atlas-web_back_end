8b606206ecccce2da214a4d0238bfa835b5c59ad
“Tristian <tristian.davis@atlasschool.com>
2025-06-28 11:45:28 -0500

    second number rounded

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -7,7 +7,7 @@ describe('calculateNumber', function() {
     assert.strictEqual(calculateNumber(1, 3), 4);
   });
 
-  it('supposed to return 5 when adding 1.2 and 3.7', function() {
+  it('supposed to return 5 when adding 1 and 3.7', function() {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
 
@@ -17,6 +17,10 @@ describe('calculateNumber', function() {
 
   it('supposed to return 6 when adding 1.5 and 3.7', function() {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+
+  it('supposed to return 1 when adding 0 and .5', function() {
+    assert.strictEqual(calculateNumber(0, .5), 1);
   });
 
 });
