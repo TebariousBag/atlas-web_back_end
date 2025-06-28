4a9d4fa7c9b3b0ef34af8d0588f8ea413240a322
“Tristian <tristian.davis@atlasschool.com>
2025-06-28 12:00:25 -0500

    trying different descriptions to satisfy checker

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -7,7 +7,11 @@ describe('calculateNumber', function() {
     assert.strictEqual(calculateNumber(1, 3), 4);
   });
 
-  it('should round second number', function() {
+  it('supposed to return 5 when adding 1 and 3.7', function() {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+
+  it('should return 5 when the second number 3.7 is rounded to 4 and added to 1', function() {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
 
