4386c5aa0b62fd35ccae33ee42142824ad81b402
“Tristian <tristian.davis@atlasschool.com>
2025-07-01 12:30:37 -0500

    usegegex in path to make sure it is a digit

--- unittests_in_js/9-api/api.js
+++ unittests_in_js/9-api/api.js
@@ -7,6 +7,11 @@ const PORT = 7865;
 app.get('/', (req, res) => {
 	res.send('Welcome to the payment system')
 });
+// cart/id endpoint, it has to be a digit, using regex for it
+app.get('/cart/:id(\\d+)', (req, res) => {
+    const id = req.params.id;
+    res.send(`Payment methods for cart ${id}`);
+});
 // send to log the port message
 app.listen(PORT, () => {
     console.log('API available on localhost port 7865')
--- unittests_in_js/9-api/api.test.js
+++ unittests_in_js/9-api/api.test.js
@@ -31,5 +31,21 @@ describe('suite for index page', function() {
       done();
     });
   });
+  // check a cart route is not a number, res 404
+  it('check if id is not a number', (done) => {
+      request('http://localhost:7865/cart/abc', (error, response, body) => {
+          assert.strictEqual(response.statusCode, 404);
+          // always remember to use done()
+          done();
+    });
+  });
+  // check a cart route is number, res with message
+  it('check if id is a number', (done) => {
+      request('http://localhost:7865/cart/456', (error, response, body) => {
+          assert.strictEqual(body, 'Payment methods for cart 456');
+          // always remember to use done()
+          done();
+    });
+  });
 
 });
