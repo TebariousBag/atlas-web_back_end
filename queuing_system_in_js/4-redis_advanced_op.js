4a68c4afba89ce3c9b894e18421b65e8a559bb34
“Tristian <tristian.davis@atlasschool.com>
2025-07-02 20:27:59 -0500

    have to import print from redis to get it to work

---
+++ queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,28 @@
+// need to import library
+import { createClient, print } from 'redis';
+// create our client
+const client = createClient();
+
+// set up on, which listens, and then give a response
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+// set up on, which listens, and then give a response
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server: ', err);
+});
+// hset format, name, field, and then the value
+client.hset('HolbertonSchools', 'Portland', '50', print);
+client.hset('HolbertonSchools', 'Seattle', '80', print);
+client.hset('HolbertonSchools', 'New York', '20', print);
+client.hset('HolbertonSchools', 'Bogota', '20', print);
+client.hset('HolbertonSchools', 'Cali', '40', print);
+client.hset('HolbertonSchools', 'Paris', '2', print);
+// hget will retrieve all the fields and values from the hash
+client.hgetall('HolbertonSchools', (err, result) => {
+  if (err) {
+    console.error('Error:', err);
+  } else {
+    console.log(result);
+  }
+});
